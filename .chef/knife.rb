7949cd5548741affc87dfe617db6cd808fe73e28
root <root@localhost.localdomain>
2017-01-02 21:24:52 -0500

    added

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,6 +6,6 @@ log_location             STDOUT
 node_name                "demo2"
 client_key               "#{current_dir}/demo2.pem"
 #chef_server_url          "https://ip-172-31-41-124.us-west-2.compute.internal/organizations/democrp"
-chef_server_url          "https://54.244.16.75/organizations/democrp"
+chef_server_url          "https://54.213.116.68/organizations/democrp"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 ssl_verify_mode    :verify_none
---
+++ cookbooks/databaseDemo/README.md
@@ -0,0 +1,4 @@
+# databaseDemo
+
+TODO: Enter the cookbook description here.
+
